fe91ef8b9e35413c78239ea869b9367747091dce
emilio barrera <barrera_emilio@yahoo.es>
2017-03-12 00:11:12 +0100

    Add project files.

---
+++ ContacBackend/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ContacBackend.MvcApplication" Language="C#" %>
---
+++ ContacBackend/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ContacBackend.Startup))]
+namespace ContacBackend
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
