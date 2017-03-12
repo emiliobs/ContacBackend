c66eafe6c8d5fdd2c7f3667cc03658f8f17d6aeb
emilio barrera <barrera_emilio@yahoo.es>
2017-03-12 02:57:59 +0100

    imagenes , helper, contactview

---
+++ ContacBackend/Class/FileHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace ContacBackend.Class
+{
+    public class FileHelper
+    {
+        public static string UploadPhoto(HttpPostedFileBase file, string folder)
+        {
+            var path = string.Empty;
+            var picture = string.Empty;
+
+            if (file != null)
+            {
+                picture = Path.GetFileName(file.FileName);
+                path = Path.Combine(HttpContext.Current.Server.MapPath(folder), picture);
+                file.SaveAs(path);
+            }
+
+            return picture;
+
+        }
+    }
+}
---
+++ ContacBackend/Models/ContactContect.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ContacBackend.Models
+{
+    public class ContactContect : DbContext
+    {
+               //Aqui me conecto a la BD:
+        public ContactContect():base("DefaultConnection")
+        {
+            
+        }
+
+        public System.Data.Entity.DbSet<ContacBackend.Models.Contact> Contacts { get; set; }
+    }
+}
